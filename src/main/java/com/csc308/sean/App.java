e0de0b3a88761d70c819820f9a795a7074ad3075
ElDanteDelAnte <sreddell@att.net>
2018-02-19 18:54:45 -0800

    Initial commit

---
+++ src/main/java/com/csc308/sean/App.java
@@ -0,0 +1,13 @@
+package com.csc308.sean;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
